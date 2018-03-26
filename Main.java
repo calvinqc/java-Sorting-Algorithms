b92b79944f920e039ae6fbd62c01833e5ea07ad4
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-03-26 13:16:58 -0700

    add insertion sort test

--- Main.java
+++ Main.java
@@ -1,33 +1,48 @@
 import BottomUpMergesort.bottomUpMergeSort;
+import InsertionSort.DoublyLinkedList;
+import InsertionSort.insertionSort;
 import quickSort.quickSort;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     public static void main(String[] args) {
+        DoublyLinkedList sortedValues;
+        insertionSort insertionSort = new insertionSort();
 
-        quickSort QuickSort = new quickSort(10000);
-        bottomUpMergeSort BotUpMergeSort = new bottomUpMergeSort(10000);
-        int[] quickSortList = QuickSort.getNumbers();
-        int[] mergeSortList = BotUpMergeSort.getOriginalList();
-        try(Scanner in = new Scanner(new File("/Users/CalvinNguyen/Desktop/Sorting-Algorithm/10,000numbers.txt"))) {
-            for (int i = 0; i < QuickSort.getNumbers().length; i++) {
-//                QuickSort.getNumbers()[i] = in.nextInt();
-                BotUpMergeSort.getOriginalList()[i] = in.nextInt();
-            }
-            BotUpMergeSort.printList();
-            BotUpMergeSort.sort(mergeSortList);
-            System.out.println(Arrays.toString(mergeSortList));
-//            QuickSort.quickSort(quickSortList);
-//            System.out.println(Arrays.toString(quickSortList));
-//            System.out.println("Print number of comparision:" + QuickSort.getTotalComparision());
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        System.out.print("Before insertion sort: ");
+        for (int k = 0; k < 10; k++) {
+            insertionSort.getList().addToFront(ThreadLocalRandom.current().nextInt(0, 5 + 1));
         }
+        System.out.println(insertionSort.getList());
+        sortedValues = insertionSort.sort();
+        System.out.print("After insertion sort: ");
+        System.out.println(sortedValues);
+
+        /** Uncomment these line to use QuickSort and Bottom-up Mergesort */
+//        quickSort QuickSort = new quickSort(10000);
+//        bottomUpMergeSort BotUpMergeSort = new bottomUpMergeSort(10000);
+//        int[] quickSortList = QuickSort.getNumbers();
+//        int[] mergeSortList = BotUpMergeSort.getOriginalList();
+//        try(Scanner in = new Scanner(new File("/Users/CalvinNguyen/Desktop/Sorting-Algorithm/10,000numbers.txt"))) {
+//            for (int i = 0; i < QuickSort.getNumbers().length; i++) {
+////                QuickSort.getNumbers()[i] = in.nextInt();
+//                BotUpMergeSort.getOriginalList()[i] = in.nextInt();
+//            }
+//            BotUpMergeSort.printList();
+//            BotUpMergeSort.sort(mergeSortList);
+//            System.out.println(Arrays.toString(mergeSortList));
+////            QuickSort.quickSort(quickSortList);
+////            System.out.println(Arrays.toString(quickSortList));
+////            System.out.println("Print number of comparision:" + QuickSort.getTotalComparision());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
