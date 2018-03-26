c2bf4331479cf87d2f51c62b0cdb7fd7ea56c8c2
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-03-26 01:22:34 -0700

    finish Bottom-up Mergesort

--- quickSort/quickSort.java
+++ quickSort/quickSort.java
@@ -8,39 +8,41 @@ import java.util.*;
  */
 public class quickSort {
 
-    int[] numbers;
-    int totalComparision;
+    private int[] numbers;
+    private int totalComparision;
 
-    quickSort(int input) {
+    public quickSort(int input) {
         numbers = new int[input];
     }
 
-    public void addTotalComparision(int input) {
+    private void addTotalComparision(int input) {
         totalComparision += input;
     }
 
-    int chooseFirstElement(int[] input, int first) {
+    private int chooseFirstElement(int[] input, int first) {
         return input[first];
     }
-    int chooseLastElement(int[] input, int last) {
+    private int chooseLastElement(int[] input, int last) {
         return input[last];
     }
-    int chooseMidEllement(int[] input, int first, int mid, int last) {
+    private int chooseMidEllement(int[] input, int first, int mid, int last) {
         int max = Math.max(input[first], Math.max(input[mid], input[last]));
         int min = Math.min(input[first], Math.min(input[mid], input[last]));
         if (input[mid] < max && input[mid] > min) return input[mid];
         if (input[first] < max && input[first] > min) return input[first];
         return input[last];
     }
-    int getMidValueindex(int first, int last) {
+
+    private int getMidValueindex(int first, int last) {
         if (first + last < 3) return first;
         return first + (last - first) / 2;
     }
 
-    void quickSort(int[] input) {
+    public void quickSort(int[] input) {
         quickSort(input, 0, input.length - 1);
     }
-    void quickSort(int[] input, int lo, int hi) {
+
+    private void quickSort(int[] input, int lo, int hi) {
         if (hi <= lo) return;
 //        int p = chooseFirstElement(input, lo);
 //        int p = chooseLastElement(input, hi);
@@ -56,7 +58,7 @@ public class quickSort {
         quickSort(input, last + 1, hi);
     }
 
-    void partition(int[] input, int low, int hi) {
+    private void partition(int[] input, int low, int hi) {
         int i = low + 1;
 //        int p = chooseFirstElement(input, low);
 //        int p = chooseLastElement(input, hi);
@@ -78,25 +80,25 @@ public class quickSort {
         swap(input, low, i - 1);
     }
 
-    void swap(int[] array, int a, int b) {
+    private void swap(int[] array, int a, int b) {
         int temp = array[b];
         array[b] = array[a];
         array[a] = temp;
     }
 
-    public static void main(String[] args)  {
-        quickSort a = new quickSort(10000);
+    public int getTotalComparision() {
+        return totalComparision;
+    }
 
-        try(Scanner in = new Scanner(new File("/Users/CalvinNguyen/Desktop/Computer Science/algorithm-Standford/assignment3.txt"))) {
-            for (int i = 0; i < a.numbers.length; i++) {
-                a.numbers[i] = in.nextInt();
-            }
-            a.quickSort(a.numbers);
-            System.out.println(Arrays.toString(a.numbers));
-            System.out.println("Print number of comparision:" + a.totalComparision);
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setTotalComparision(int totalComparision) {
+        this.totalComparision = totalComparision;
     }
 }
